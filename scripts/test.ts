530ae48f01e6f73f0f4551ea983a748624688b20
mthierman <mthierman@gmail.com>
2024-07-14 09:29:31 -0600

    Add test script

---
+++ scripts/test.ts
@@ -0,0 +1,36 @@
+import Color from "colorjs.io";
+import resolveConfig from "tailwindcss/resolveConfig.js";
+import type { DefaultColors, Theme, ThemeInfo } from "../modules/types.mjs";
+import tailwindConfig from "../tailwind.config.js";
+
+type ConvertParams = {
+    colors: Record<string, Color | string>;
+    format: "hex" | "Color";
+};
+
+function convertColor(colors: Record<string, Color | string>, format: "hex" | "Color") {
+    Object.entries(colors).forEach(([key, value]: [string, Color | string]) => {
+        switch (format) {
+            case "hex":
+                {
+                    colors[key] = value.toString({ format: "hex" });
+                }
+                break;
+            case "Color":
+                {
+                    colors[key] = new Color(value);
+                }
+                break;
+        }
+    });
+    return colors;
+}
+
+type Colors = Omit<DefaultColors, "inherit" | "current" | "transparent" | "black" | "white">;
+type Tailwind = Colors & { transparent?: "#00000000" };
+
+const { inherit, current, transparent, black, white, ...colors } =
+    resolveConfig(tailwindConfig).theme.colors;
+const tailwind: Tailwind = { ...colors };
+
+console.log(convertColor(tailwind.amber, "Color"));
