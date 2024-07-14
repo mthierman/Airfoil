34e1bb07f65c4df4fade940df76b0dadb9b1cf9e
mthierman <mthierman@gmail.com>
2024-07-13 21:58:50 -0600

    Fix build script

--- scripts/airfoil.ts
+++ scripts/airfoil.ts
@@ -1128,14 +1128,14 @@ const outdir = {
     terminal: resolve(import.meta.dirname, "..", "..", "terminal"),
 };
 
-await mkdir(outdir.themes, { recursive: true });
-
 const themes = manifest.contributes.themes as ThemeManifest[];
 
-let promise: Promise<void>[] = [];
+await mkdir(outdir.themes, { recursive: true });
+
+let themePromises: Promise<void>[] = [];
 
 for (const theme of themes) {
-    promise.push(
+    themePromises.push(
         writeFile(
             resolve(import.meta.dirname, "..", "..", theme.path),
             JSON.stringify(generateTheme(makeTheme(theme)), null, 4),
@@ -1143,33 +1143,32 @@ for (const theme of themes) {
     );
 }
 
-Promise.all(promise);
+Promise.all(themePromises);
 
-// mkdir(outdir.terminal, { recursive: true })
-//     .then(() => {
-//         let promise: Promise<void>[] = [];
-//         let all: unknown[] = [];
+await mkdir(outdir.terminal, { recursive: true });
 
-//         for (const theme of themes) {
-//             promise.push(
-//                 writeFile(
-//                     resolve(
-//                         import.meta.dirname,
-//                         "..",
-//                         theme.path.replace("./themes/", "./terminal/"),
-//                     ),
-//                     JSON.stringify(generateTerminal(makeTheme(theme)), null, 4),
-//                 ),
-//             );
+let terminalPromises: Promise<void>[] = [];
+let allTerminalThemes: unknown[] = [];
 
-//             all.push(generateTerminal(makeTheme(theme)));
-//         }
+for (const theme of themes) {
+    terminalPromises.push(
+        writeFile(
+            resolve(
+                import.meta.dirname,
+                "..",
+                "..",
+                theme.path.replace("./themes/", "./terminal/"),
+            ),
+            JSON.stringify(generateTerminal(makeTheme(theme)), null, 4),
+        ),
+    );
 
-//         Promise.all(promise).then(() => {
-//             writeFile(
-//                 resolve(import.meta.dirname, "..", "terminal", "all.json"),
-//                 JSON.stringify(all, null, 4),
-//             );
-//         });
-//     })
-//     .catch(() => process.exit(1));
+    allTerminalThemes.push(generateTerminal(makeTheme(theme)));
+}
+
+await Promise.all(terminalPromises);
+
+writeFile(
+    resolve(import.meta.dirname, "..", "..", "terminal", "all.json"),
+    JSON.stringify(allTerminalThemes, null, 4),
+);
