42ef8b1614f75e89267cc315a24d2a11cd0039f5
mthierman <mthierman@gmail.com>
2024-07-13 21:30:36 -0600

    Simplify manifest writeFile

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -45,44 +45,9 @@ writeFile(
     resolve(import.meta.dirname, "..", "package.json"),
     JSON.stringify(
         {
-            name: "theme-airfoil",
-            version: "0.0.1",
-            private: true,
-            type: "module",
-            publisher: "mthierman",
-            engines: {
-                vscode: "*",
-            },
-            license: "MIT",
-            displayName: "Airfoil Theme",
-            description: "Airfoil light & dark themes",
-            categories: ["Themes"],
-            keywords: ["theme", "airfoil", "dark", "light"],
-            galleryBanner: {
-                color: "#000000",
-                theme: "dark",
-            },
+            ...manifest,
             contributes: {
                 themes: [...themes],
-            },
-            icon: "data/icon.png",
-            homepage: "https://github.com/mthierman/Airfoil/blob/main/README.md",
-            bugs: {
-                email: "mthierman@gmail.com",
-                url: "https://github.com/mthierman/Airfoil/issues",
-            },
-            repository: {
-                type: "git",
-                url: "https://github.com/mthierman/Airfoil.git",
-            },
-            scripts: {
-                check: "tsc",
-                dev: "esbuild scripts/dev.ts --bundle --outdir=build --platform=node --format=esm --packages=external && node build/dev.js",
-                build: "tsc && esbuild scripts/build.ts --bundle --outdir=build --platform=node --format=esm --packages=external && node build/build.js",
-                vsce: "vsce publish",
-            },
-            devDependencies: {
-                ...manifest.devDependencies,
             },
         },
         null,
