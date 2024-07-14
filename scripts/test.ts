3bf2447b13cbb41321d0ef55fb0d55aa53ccd1f9
mthierman <mthierman@gmail.com>
2024-07-14 09:51:45 -0600

    Checkpoint test

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,31 +1,62 @@
 import Color from "colorjs.io";
 import resolveConfig from "tailwindcss/resolveConfig.js";
-import type { DefaultColors, Theme, ThemeInfo } from "../modules/types.mjs";
+import type { DefaultColors } from "../modules/types.mjs";
 import tailwindConfig from "../tailwind.config.js";
 
-function convertColor(colors: Record<string, Color | string>, format: "hex" | "Color") {
+type Tailwind = Omit<DefaultColors, "inherit" | "current" | "transparent" | "black" | "white"> & {
+    transparent?: "#00000000";
+};
+
+export type Scale =
+    | "50"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900"
+    | "950";
+
+export type Tone = "Slate" | "Gray" | "Zinc" | "Neutral" | "Stone";
+
+export type Accent =
+    | "Red"
+    | "Orange"
+    | "Amber"
+    | "Yellow"
+    | "Lime"
+    | "Green"
+    | "Emerald"
+    | "Teal"
+    | "Cyan"
+    | "Sky"
+    | "Blue"
+    | "Indigo"
+    | "Violet"
+    | "Purple"
+    | "Fuchsia"
+    | "Pink"
+    | "Rose";
+
+function convertColor(colors: Record<string, Color | string>) {
     Object.entries(colors).forEach(([key, value]: [string, Color | string]) => {
-        switch (format) {
-            case "hex":
-                {
-                    colors[key] = value.toString({ format: "hex" });
-                }
-                break;
-            case "Color":
-                {
-                    colors[key] = new Color(value);
-                }
-                break;
+        if (value instanceof Color) {
+            colors[key] = value.toString({ format: "hex" });
+        }
+        if (typeof value === "string") {
+            colors[key] = new Color(value);
         }
     });
     return colors;
 }
 
-type Colors = Omit<DefaultColors, "inherit" | "current" | "transparent" | "black" | "white">;
-type Tailwind = Colors & { transparent?: "#00000000" };
-
 const { inherit, current, transparent, black, white, ...colors } =
     resolveConfig(tailwindConfig).theme.colors;
 const tailwind: Tailwind = { ...colors };
 
-console.log(convertColor(tailwind.amber, "Color"));
+// console.log(convertColor(tailwind));
+const amber = convertColor(tailwind.amber);
+console.log(amber);
