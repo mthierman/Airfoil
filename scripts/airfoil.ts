bc696851429ff13821b1b9d7c80f9763698d89ad
mthierman <mthierman@gmail.com>
2024-07-13 21:54:32 -0600

    Fix paths

--- scripts/airfoil.ts
+++ scripts/airfoil.ts
@@ -1124,54 +1124,52 @@ const generateTheme = (theme: Theme) => {
 };
 
 const outdir = {
-    themes: resolve(import.meta.dirname, "..", "themes"),
-    terminal: resolve(import.meta.dirname, "..", "terminal"),
+    themes: resolve(import.meta.dirname, "..", "..", "themes"),
+    terminal: resolve(import.meta.dirname, "..", "..", "terminal"),
 };
+
+await mkdir(outdir.themes, { recursive: true });
 
 const themes = manifest.contributes.themes as ThemeManifest[];
 
-mkdir(outdir.themes, { recursive: true })
-    .then(() => {
-        let promise: Promise<void>[] = [];
+let promise: Promise<void>[] = [];
 
-        for (const theme of themes) {
-            promise.push(
-                writeFile(
-                    resolve(import.meta.dirname, "..", theme.path),
-                    JSON.stringify(generateTheme(makeTheme(theme)), null, 4),
-                ),
-            );
-        }
+for (const theme of themes) {
+    promise.push(
+        writeFile(
+            resolve(import.meta.dirname, "..", "..", theme.path),
+            JSON.stringify(generateTheme(makeTheme(theme)), null, 4),
+        ),
+    );
+}
 
-        Promise.all(promise);
-    })
-    .catch(() => process.exit(1));
+Promise.all(promise);
 
-mkdir(outdir.terminal, { recursive: true })
-    .then(() => {
-        let promise: Promise<void>[] = [];
-        let all: unknown[] = [];
+// mkdir(outdir.terminal, { recursive: true })
+//     .then(() => {
+//         let promise: Promise<void>[] = [];
+//         let all: unknown[] = [];
 
-        for (const theme of themes) {
-            promise.push(
-                writeFile(
-                    resolve(
-                        import.meta.dirname,
-                        "..",
-                        theme.path.replace("./themes/", "./terminal/"),
-                    ),
-                    JSON.stringify(generateTerminal(makeTheme(theme)), null, 4),
-                ),
-            );
+//         for (const theme of themes) {
+//             promise.push(
+//                 writeFile(
+//                     resolve(
+//                         import.meta.dirname,
+//                         "..",
+//                         theme.path.replace("./themes/", "./terminal/"),
+//                     ),
+//                     JSON.stringify(generateTerminal(makeTheme(theme)), null, 4),
+//                 ),
+//             );
 
-            all.push(generateTerminal(makeTheme(theme)));
-        }
+//             all.push(generateTerminal(makeTheme(theme)));
+//         }
 
-        Promise.all(promise).then(() => {
-            writeFile(
-                resolve(import.meta.dirname, "..", "terminal", "all.json"),
-                JSON.stringify(all, null, 4),
-            );
-        });
-    })
-    .catch(() => process.exit(1));
+//         Promise.all(promise).then(() => {
+//             writeFile(
+//                 resolve(import.meta.dirname, "..", "terminal", "all.json"),
+//                 JSON.stringify(all, null, 4),
+//             );
+//         });
+//     })
+//     .catch(() => process.exit(1));
