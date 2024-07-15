748182b07665fb30e3c9c774bc05824c179f13fe
mthierman <mthierman@gmail.com>
2024-07-15 16:55:24 -0600

    Update manifest

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -2,4 +2,6 @@ import { writeFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import { generateManifest } from "../modules/generators.mjs";
 
+console.log(resolve(import.meta.dirname, "..", "..", "package.json"));
+
 await writeFile(resolve(import.meta.dirname, "..", "package.json"), generateManifest());
