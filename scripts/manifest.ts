a0501466847d85bf9fac6382e913239118cd7f5b
mthierman <mthierman@gmail.com>
2024-07-15 16:55:51 -0600

    Cleanup

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -2,6 +2,4 @@ import { writeFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import { generateManifest } from "../modules/generators.mjs";
 
-console.log(resolve(import.meta.dirname, "..", "..", "package.json"));
-
 await writeFile(resolve(import.meta.dirname, "..", "package.json"), generateManifest());
