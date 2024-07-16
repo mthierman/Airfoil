88d6e5696816c316f4c5c50fc66c9625b30543f8
mthierman <mthierman@gmail.com>
2024-07-15 19:32:33 -0600

    Re-add themes to build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,33 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import { generateTerminal } from "../modules/generators.mjs";
+import manifest from "../package.json" with { type: "json" };
 
 const outdir = {
     themes: resolve(import.meta.dirname, "..", "..", "themes"),
     terminal: resolve(import.meta.dirname, "..", "..", "terminal"),
 };
+
+// ======
+
+await mkdir(outdir.themes, { recursive: true });
+
+const themes = manifest.contributes.themes;
+
+let themePromises: Promise<void>[] = [];
+
+for (const theme of themes) {
+    themePromises.push(
+        writeFile(
+            resolve(import.meta.dirname, "..", "..", theme.path),
+            JSON.stringify(generateTheme(makeTheme(theme)), null, 4),
+        ),
+    );
+}
+
+await Promise.all(themePromises);
+
+// ======
 
 await mkdir(outdir.terminal, { recursive: true });
 
